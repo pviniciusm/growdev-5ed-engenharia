3dd520034b067910cae2c9e939330d99e93f875d
Crhistian <crhistiandeoliveira@gmail.com>
2022-05-05 22:09:27 -0300

    add rota growcoins

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import cors from "cors";
 
 const app = express();
@@ -12,6 +12,33 @@ app.get("/", (_, res) => {
         samuelAddams: {
             teste: "ABC samuel addams",
         },
+    });
+});
+
+app.get("/growcoins/:growcoins", (req: Request, res: Response) => {
+    const growcoins = req.params.growcoins as string;
+    const min: number = 1 
+    const max: number = 100
+    const result: number = Math.floor(Math.random() * (max - min + 1)) + min;
+    const response = result * Number(growcoins)
+    result === 100 
+    ? res.status(200).send({
+        ok: true,
+        message: `Seu número aleatório é 100!!! Você ganhou o triplo de GrowCoins: ${response*3}!!!`,
+    })
+    : result > 50 && result !== 100
+    ? res.status(200).send({
+        ok: true,
+        message: `Seu número aleatório maior que 50 é ${result}, você ganhou ${response} growcoins.`,
+    })
+    : result === 50 
+    ? res.status(200).send({
+        ok: true,
+        message: `Seu número aleatório é exatamente 50! Você ganhou o dobro: ${response*2} GrowCoins`,
+    })
+    : res.status(418).send({
+        ok: false,
+        message: `Seu número aleatório é muito baixo, você perdeu todas as suas GrowCoins :(`,
     });
 });
 
