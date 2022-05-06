5e04bc8b222e0801b19b1a92ab42ee3210406800
JAMERSON FELIPE PEREIRA PAZ <86246952+Ljames666@users.noreply.github.com>
2022-05-05 21:58:42 -0300

    eu add delete

--- src/main.ts
+++ src/main.ts
@@ -1,38 +1,46 @@
-import express from "express";
-import cors from "cors";
+import express from 'express';
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (_, res) => {
-    res.send({
-        ok: false,
-        message: "it is ok",
-        samuelAddams: {
-            teste: "ABC samuel addams",
-        },
-    });
+app.get('/', (_, res) => {
+  res.send({
+    ok: false,
+    message: 'it is ok',
+    samuelAddams: {
+      teste: 'ABC samuel addams',
+    },
+  });
 });
 
-app.post("/", (_, res) => {
-    res.send({
-        ok: true,
-        message: "it is ok",
-        samuelAddams: {
-            teste: "abc samuel addams",
-        },
-    });
+app.post('/', (_, res) => {
+  res.send({
+    ok: true,
+    message: 'it is ok',
+    samuelAddams: {
+      teste: 'abc samuel addams',
+    },
+  });
 });
 
-app.put("/", (_, res) => {
-    res.send({
-        ok: true,
-        message: "it is ok",
-        samuelAddams: {
-            teste: "abc samuel addams",
-        },
-    });
+app.put('/', (_, res) => {
+  res.send({
+    ok: true,
+    message: 'it is ok',
+    samuelAddams: {
+      teste: 'abc samuel addams',
+    },
+  });
+});
+app.delete('/', (_, res) => {
+  res.send({
+    ok: true,
+    message: 'arquivo deletado',
+  });
 });
 
-app.listen(8081, () => console.log("Server is running..."));
+// jamereson é o cara kkkkkk
+
+app.listen(8081, () => console.log('Server is running...'));
